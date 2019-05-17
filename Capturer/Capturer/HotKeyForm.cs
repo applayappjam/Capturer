cacae44d8561a1add6f01819638562fa37613ef6
KIMSHINWOO <swkim9985@naver.com>
2019-05-18 00:46:25 +0900

    Update HotKeyForm.cs

--- Capturer/Capturer/HotKeyForm.cs
+++ Capturer/Capturer/HotKeyForm.cs
@@ -28,15 +28,24 @@ namespace Capturer
         {
             Keys key = e.KeyCode;
             string stkey = key.ToString();
-            string CtrKey = "Ctrl + " + stkey;          
             if (key == Keys.Escape)
             {
-                MessageBox.Show("Esc는 단축키로 지정할 수 없습니다!");                
+                MessageBox.Show("Esc는 단축키로 지정할 수 없습니다!");
             }
             //Ctrl + 키 입력시
-            else if (e.Control == true)
+            else if ((Control.ModifierKeys & Keys.Control) == Keys.Control)
             {
-                this.NewHotKey.Text = CtrKey;
+                this.NewHotKey.Text = Keys.Control + " + " + stkey;
+            }
+            //Shift + 키 입력시
+            else if ((Control.ModifierKeys & Keys.Shift) == Keys.Shift)
+            {
+                this.NewHotKey.Text = Keys.Shift + " + " + stkey;
+            }
+            //Alt + 키 입력시
+            else if ((Control.ModifierKeys & Keys.Alt) == Keys.Alt)
+            {
+                this.NewHotKey.Text = Keys.Alt + " + " + stkey;
             }
             //키 입력시
             else
