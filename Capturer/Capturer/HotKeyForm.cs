59b02065329e25aaa4121adb7eb23391550facb5
KIMSHINWOO <swkim9985@naver.com>
2019-05-17 21:16:16 +0900

    Update HotKeyForm.cs

--- Capturer/Capturer/HotKeyForm.cs
+++ Capturer/Capturer/HotKeyForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Capturer;
 
 namespace Capturer
 {
@@ -15,6 +16,39 @@ namespace Capturer
         public HotKeyForm()
         {
             InitializeComponent();
+            this.currentHotKey.Text = MainForm.status.SelectedHotKey;
+        }
+
+        private void HotKeyForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyDownHotKey(object sender, KeyEventArgs e)
+        {
+            Keys key = e.KeyCode;
+            string stkey = key.ToString();
+            string CtrKey = "Ctrl + " + stkey;          
+            if (key == Keys.Escape)
+            {
+                MessageBox.Show("Esc는 단축키로 지정할 수 없습니다!");                
+            }
+            //Ctrl + 키 입력시
+            else if (e.Control == true)
+            {
+                this.NewHotKey.Text = CtrKey;
+            }
+            //키 입력시
+            else
+                this.NewHotKey.Text = stkey;
+        }
+
+        private void ChangeHotKey(object sender, EventArgs e)
+        {
+            this.currentHotKey.Text = this.NewHotKey.Text;
+            Capturer.MainForm.status.SelectedHotKey = currentHotKey.Text;
+            MessageBox.Show("변경되었습니다!");
+
         }
     }
 }
